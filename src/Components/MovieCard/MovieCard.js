492b007870e9cabdf5eccdf0a70a57ab000859c4
Edward Stoner <edwardlstoner@gmail.com>
2020-04-08 15:45:58 -0600

    Add movie card JSX

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import './MovieCard.css'
+import React from "react";
+import "./MovieCard.css";
 
 const MovieCard = ({ title, poster_path, average_rating }) => {
-  return(
-    <div>
-      <h2>{title}</h2>
-      <img src={poster_path}></img>
-      <p>{average_rating}</p>
+  return (
+    <div
+      className="movie-card"
+      style={{
+        background: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 1)), url(${poster_path})`,
+        "background-size": "cover",
+      }}
+    >
+      {/* <img className="card-image" src={poster_path}></img> */}
+      <div className="title-rating-container">
+        <h2 className="movie-card-title">{title}</h2>
+        <p className="movie-card-rating">
+          <span className="rating-score">
+            {average_rating > 5 ? (
+              <i class="fas fa-smile"></i>
+            ) : (
+              <i class="fas fa-meh"></i>
+            )}
+            {average_rating}
+          </span>{" "}
+          out of 10
+        </p>
+      </div>
     </div>
   );
 };
