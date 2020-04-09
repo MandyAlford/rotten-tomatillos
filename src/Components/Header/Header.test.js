90e7b9b92b93221135afce7f85286d1ffc6bab1b
Edward Stoner <edwardlstoner@gmail.com>
2020-04-09 14:25:04 -0600

    Add header tests

---
+++ src/Components/Header/Header.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Header from "./Header";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("header", () => {
+  it("should render the text we expect", () => {
+    const { getByText } = render(<Header user={"Ed"} />);
+    const title = getByText("Rancid Tomatillos");
+    const signOut = getByText("Sign Out");
+    expect(title).toBeInTheDocument();
+    expect(signOut).toBeInTheDocument();
+  });
+
+  it("should fire correct function when log out button clicked", () => {
+    const mockLogOut = jest.fn();
+    const { getByText, debug } = render(
+      <Header
+        user={{
+          name: "Ed",
+          id: 2,
+          email: "ed@ed.com",
+        }}
+        logout={mockLogOut}
+      />
+    );
+    const signOut = getByText("Sign Out");
+    fireEvent.click(signOut);
+    expect(mockLogOut).toBeCalledTimes(1);
+  });
+
+  it("should fire correct function when log in button clicked", () => {
+    const mockShowModal = jest.fn();
+    const { getByText } = render(
+      <Header
+        user={{
+          name: "",
+          id: null,
+          email: "",
+        }}
+        showModal={mockShowModal}
+      />
+    );
+    const signIn = getByText("Sign In");
+    fireEvent.click(signIn);
+    expect(mockShowModal).toBeCalledTimes(1);
+  });
+});
