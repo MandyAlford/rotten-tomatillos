43a26516bb414aeda7fd85c1581fe0af52a65b31
Edward Stoner <edwardlstoner@gmail.com>
2020-04-08 20:41:48 -0600

    Add Link to MovieCard

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./MovieCard.css";
+import { Link } from "react-router-dom";
 
-const MovieCard = ({ title, poster_path, average_rating }) => {
+const MovieCard = ({ title, poster_path, average_rating, id }) => {
   return (
-    <div
+    <Link
+      to={"/movies/" + id}
       className="movie-card"
       style={{
         background: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 1)), url(${poster_path})`,
         "background-size": "cover",
       }}
     >
-      {/* <img className="card-image" src={poster_path}></img> */}
       <div className="title-rating-container">
         <h2 className="movie-card-title">{title}</h2>
         <p className="movie-card-rating">
           <span className="rating-score">
-            {average_rating > 5 ? (
+            {average_rating >= 5 ? (
               <i class="fas fa-smile"></i>
             ) : (
               <i class="fas fa-meh"></i>
@@ -25,7 +26,7 @@ const MovieCard = ({ title, poster_path, average_rating }) => {
           out of 10
         </p>
       </div>
-    </div>
+    </Link>
   );
 };
 
