477bb85f035941ec0226b743aac4a69ac64dba9b
Karen Nguyen <knguyen4184@yahoo.com>
2020-04-09 20:21:24 -0600

    Style movieDetail component

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { getMovieDetails } from '../../ApiCalls/ApiCalls'
+import './MovieDetails.css'
 
 
 class MovieDetails extends Component {
   constructor(){
     super()
     this.state= {
-      movie: null
+      movie: null,
     }
   }
 
@@ -18,26 +19,31 @@ class MovieDetails extends Component {
 
   render() {
     return(
-      <div className="movie-detail-container" style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}}>
+    <div className="movie-detail-container" >
+      <div className="movie-detail-backdrop" 
+       style={{
+        backgroundImage: `url(${
+          this.state.movie && this.state.movie.backdrop_path
+        })`,
+      }}></div>
       {this.state.movie &&
         <div>
-          <h2 className="movie-detail-title">{this.state.movie.title}</h2>
           <img 
           className="movie-detail-poster" 
           src={this.state.movie.poster_path} 
           alt= {`${this.state.movie.title} movie poster`}
           />
-          {/* <img 
-          className="movie-detail-backdrop" 
-          src={this.state.movie.backdrop_path} 
-          alt={`${this.state.movie.title} movie backdrop`}
-          /> */}
-          <p className="movie-detail-release">{this.state.movie.release_date}</p>
-          <p className="movie-detail-overview">{this.state.movie.overview}</p>
-          <p className="movie-detail-rating">{this.state.movie.average_rating}</p>
         </div>
       }
-      </div>
+      {this.state.movie &&
+        <div>
+          <h2 className="movie-detail-title">{this.state.movie.title}</h2>
+          <p className="movie-detail-release">Release Date: {this.state.movie.release_date}</p>
+          <p className="movie-detail-overview">{this.state.movie.overview}</p>
+          <p className="movie-detail-rating">Average Rating: {this.state.movie.average_rating}</p>
+        </div>
+      }
+    </div>
     )
   }
 }
