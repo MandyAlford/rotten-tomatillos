5a844bb46fb7a9ea49ed507866ffea2a696ab78d
Edward Stoner <edwardlstoner@gmail.com>
2020-04-11 15:19:42 -0600

    Add clear ratings to reducer

--- src/reducers/ratings.js
+++ src/reducers/ratings.js
@@ -2,6 +2,8 @@ export const ratings = (state = [], action) => {
   switch (action.type) {
     case "GET_USER_RATINGS":
       return action.ratings;
+    case "CLEAR_RATINGS":
+      return [];
     default:
       return state;
   }
