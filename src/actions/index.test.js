da5c8b7cdf9d3d02888224faec661f99f3a43b1b
Mandy Alford <mandyalford@gmail.com>
2020-04-11 14:07:49 -0600

    Write and pass tests for GET_MOVIES action

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -90,4 +90,50 @@ describe("actions", () => {
 
     expect(result).toEqual(expectedAction);
   });
+
+  it("should have a type of GET_MOVIES and correct payload", () => {
+    const expectedAction = {
+      type: "GET_MOVIES",
+      movies: [{
+        id: 1,
+        title: "Bloodshot",
+        poster_path: "https://image.tmdb.org/t/p/original//8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//ocUrMYbdjknu2TwzMHKT9PBBQRw.jpg",
+        release_date: "2020-03-05",
+        overview: "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists.",
+        average_rating: 6
+      },
+      {
+        id: 2,
+        title: "Sonic the Hedgehog",
+        poster_path: "https://image.tmdb.org/t/p/original//aQvJ5WPzZgYVDrxLX4R6cLJCEaQ.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//stmYfCUGd8Iy6kAMBr6AmWqx8Bq.jpg",
+        release_date: "2020-02-12",
+        overview: "Based on the global blockbuster videogame franchise from Sega.",
+        average_rating: 4.333333333333333
+      }
+      ]
+    };
+
+      const result = actions.getMovies([{
+        id: 1,
+        title: "Bloodshot",
+        poster_path: "https://image.tmdb.org/t/p/original//8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//ocUrMYbdjknu2TwzMHKT9PBBQRw.jpg",
+        release_date: "2020-03-05",
+        overview: "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists.",
+        average_rating: 6
+      },
+      {
+        id: 2,
+        title: "Sonic the Hedgehog",
+        poster_path: "https://image.tmdb.org/t/p/original//aQvJ5WPzZgYVDrxLX4R6cLJCEaQ.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//stmYfCUGd8Iy6kAMBr6AmWqx8Bq.jpg",
+        release_date: "2020-02-12",
+        overview: "Based on the global blockbuster videogame franchise from Sega.",
+        average_rating: 4.333333333333333
+      }
+      ])
+      expect(result).toEqual(expectedAction);
+  });
 });
