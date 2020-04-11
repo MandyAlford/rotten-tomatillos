9d5930413ac88c625cd84a4db8ac01aa3ebd9604
Edward Stoner <edwardlstoner@gmail.com>
2020-04-10 21:40:44 -0600

    Add API calls for movie ratings

--- src/ApiCalls/ApiCalls.js
+++ src/ApiCalls/ApiCalls.js
@@ -4,15 +4,35 @@ export const fetchUserLogin = (userData) => {
   return fetch(BASE_URL, {
     method: "Post",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(userData)
+    body: JSON.stringify(userData),
   })
-    .then(response => response.json())
-    .then(data => data);
+    .then((response) => response.json())
+    .then((data) => data);
 };
 
 export const getMovieDetails = () => {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
-    .then(response => response.json());
+  return fetch(
+    "https://rancid-tomatillos.herokuapp.com/api/v1/movies"
+  ).then((response) => response.json());
+};
+
+export const fetchUserRatings = (userId) => {
+  return fetch(
+    `https://rancid-tomatillos.herokuapp.com/api/v1/users/${userId}/ratings`
+  ).then((response) => response.json());
+};
+
+export const submitRating = (userId, movieId, rating) => {
+  return fetch(
+    `https://rancid-tomatillos.herokuapp.com/api/v1/users/${userId}/ratings`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ movie_id: movieId, rating: rating }),
+    }
+  ).then((response) => response.json());
 };
