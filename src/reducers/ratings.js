8b3b0fc4ae3793cd21f149eb435165b7e9d9ae87
Edward Stoner <edwardlstoner@gmail.com>
2020-04-10 21:55:02 -0600

    Remove unused action

--- src/reducers/ratings.js
+++ src/reducers/ratings.js
@@ -1,7 +1,5 @@
 export const ratings = (state = [], action) => {
   switch (action.type) {
-    case "RATE_MOVIE":
-      return [...state, action.rating];
     case "GET_USER_RATINGS":
       return action.ratings;
     default:
