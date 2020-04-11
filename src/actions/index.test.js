be2afba37f8fca74fb0b562368fde33cce2c42d0
Erik Raith <raithhtiar@gmail.com>
2020-04-11 12:55:12 -0600

    [hotfix] update index.test syntax

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -4,13 +4,13 @@ describe('actions', () => {
   it('should have a type of LOGIN_USER and a correct payload', () => {
     const expectedAction = {
       type: 'LOGIN_USER',
-      userData: {
+
         user: {
           id: 4,
           name: "Greg",
           email: "greg@turing.io"
         }
-      }
+
     };
 
     const result = actions.login(
