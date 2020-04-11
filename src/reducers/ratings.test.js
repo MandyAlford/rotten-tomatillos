0f3a9831e70297553ddfacaf81a3fdbf8c789e03
Mandy Alford <mandyalford@gmail.com>
2020-04-11 13:50:30 -0600

    Write and pass test for returning updated state in ratings reducer

--- src/reducers/ratings.test.js
+++ src/reducers/ratings.test.js
@@ -9,4 +9,35 @@ describe('Reducer: ratings', () => {
 
     expect(result).toEqual(expected);
   });
+
+  it('should return updated state', () => {
+    const expected = [
+      {
+      id: 63,
+      user_id: 4,
+      movie_id: 4,
+      rating: 5
+      },
+      {
+      id: 66,
+      user_id: 4,
+      movie_id: 3,
+      rating: 4,
+      },
+      {
+      id: 65,
+      user_id: 4,
+      movie_id: 5,
+      rating: 4
+      },
+    ];
+    const action = {
+      type:'GET_USER_RATINGS',
+      ratings: expected
+    }
+
+    const result = ratings([], action);
+
+    expect(result).toEqual(expected);
+  });
 });
