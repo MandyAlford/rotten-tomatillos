7f39305a792e21740c02b3269d23794c64a2a8e3
Edward Stoner <edwardlstoner@gmail.com>
2020-04-11 12:11:42 -0600

    Remove state from app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,62 +1,41 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
-import {bindActionCreators} from 'redux';
+import { bindActionCreators } from "redux";
 import "./App.css";
 import Login from "../Login/Login";
 import MoviesContainer from "../MoviesContainer/MoviesContainer";
 import Header from "../Header/Header";
-import MovieDetails from '../MovieDetails/MovieDetails';
-import {showModal} from '../../actions'
+import MovieDetails from "../MovieDetails/MovieDetails";
+import { getMovies } from "../../actions";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: [],
-      user: {
-        name: "",
-        id: null,
-        email: "",
-      },
-      showLoginModal: false,
-    };
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount = () => {
+    const { getMovies } = this.props;
     fetch("https://rancid-tomatillos.herokuapp.com/api/v1/movies")
       .then((response) => response.json())
-      .then((movies) => this.setState({ movies: movies.movies }));
-  };
-
-  logout = () => {
-    this.setState({ user: { name: "", id: null, email: "" } });
+      .then((movies) => getMovies(movies.movies));
   };
 
   render() {
     return (
-    <div>
-      <Route path='/' exact>
-        <Header
-        />
-        <Login login={this.login} showLoginModal={this.state.showLoginModal} />
-        <MoviesContainer
-        movies={this.state.movies}
-        />
-      </Route>
-
-      <Route path='/movies/:movie_id' exact component={MovieDetails}>
-      </Route>
-    </div>
+      <div>
+        <Route path="/" exact>
+          <Header />
+          <Login />
+          <MoviesContainer />
+        </Route>
+        <Route path="/movies/:movie_id" exact component={MovieDetails}></Route>
+      </div>
     );
   }
 }
 
-const mapStateToProps = ({user,showLoginModal}) => ({
-  user,
-  showLoginModal
-});
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ getMovies }, dispatch);
 
-const mapDispatchToProps = dispatch => ( bindActionCreators({showModal},dispatch));
-
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(null, mapDispatchToProps)(App);
