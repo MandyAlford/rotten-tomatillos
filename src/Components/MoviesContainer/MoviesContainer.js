fa47e188d9d81703fb45d8cd2e0c6d6decce47a8
Edward Stoner <edwardlstoner@gmail.com>
2020-04-11 12:11:57 -0600

    Map state to props

--- src/Components/MoviesContainer/MoviesContainer.js
+++ src/Components/MoviesContainer/MoviesContainer.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import MovieCard from '../MovieCard/MovieCard'
-import './MoviesContainer.css';
-import MovieDetails from '../MovieDetails/MovieDetails';
-
+import React from "react";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MoviesContainer.css";
+import MovieDetails from "../MovieDetails/MovieDetails";
+import { connect } from "react-redux";
 
 const MoviesContainer = ({ movies }) => {
+  console.log(movies);
   const cards = movies.map((movie) => {
-    return (
-      <MovieCard
-        key={movie.id}
-        movie={movie}
-      />
-    );
+    return <MovieCard key={movie.id} movie={movie} />;
   });
   return <div className="cards-container">{cards}</div>;
 };
 
-export default MoviesContainer;
+const mapStateToProps = (state) => ({
+  movies: state.movies,
+});
+
+export default connect(mapStateToProps)(MoviesContainer);
