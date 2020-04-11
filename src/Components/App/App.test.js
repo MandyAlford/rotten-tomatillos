e01533e1a7d9fc235986f3317df4f69d3f00fdb9
Edward Stoner <edwardlstoner@gmail.com>
2020-04-11 12:42:30 -0600

    Remove default test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
-});
+test("renders learn react link", () => {});
