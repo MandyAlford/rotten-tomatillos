f9b7ac9d2679f9bafa165f5030b2a5fc8ec57a12
Edward Stoner <edwardlstoner@gmail.com>
2020-04-13 16:33:15 -0600

    Refactor MoviesContainer tests

--- src/Components/MoviesContainer/MoviesContainer.test.js
+++ src/Components/MoviesContainer/MoviesContainer.test.js
@@ -4,33 +4,39 @@ import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "../../reducers";
+
 describe("Movies Container", () => {
   it("should render the text we expect", () => {
+    const store = createStore(rootReducer);
+
     const { getByText } = render(
-      <BrowserRouter>
-        <MoviesContainer
-          movies={[
-            {
-              id: 15,
-              title: "Just Mercy",
-              poster_path:
-                "https://image.tmdb.org/t/p/original//4YJNp1cquIkX8JxFwkKNEFQ9tgr.jpg",
-              backdrop_path:
-                "https://image.tmdb.org/t/p/original//zNqsGEWHlXwFw8wod0HDkgrvsl8.jpg",
-              release_date: "2019-12-25",
-              overview:
-                "The powerful true story of Harvard-educated lawyer Bryan Stevenson, who goes to Alabama to defend the disenfranchised and wrongly condemned — including Walter McMillian, a man sentenced to death despite evidence proving his innocence. Bryan fights tirelessly for Walter with the system stacked against them.",
-              average_rating: 7.5,
-            },
-          ]}
-        />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <MoviesContainer
+            movies={[
+              {
+                id: 15,
+                title: "Just Mercy",
+                poster_path:
+                  "https://image.tmdb.org/t/p/original//4YJNp1cquIkX8JxFwkKNEFQ9tgr.jpg",
+                backdrop_path:
+                  "https://image.tmdb.org/t/p/original//zNqsGEWHlXwFw8wod0HDkgrvsl8.jpg",
+                release_date: "2019-12-25",
+                overview:
+                  "The powerful true story of Harvard-educated lawyer Bryan Stevenson, who goes to Alabama to defend the disenfranchised and wrongly condemned — including Walter McMillian, a man sentenced to death despite evidence proving his innocence. Bryan fights tirelessly for Walter with the system stacked against them.",
+                average_rating: 7.5,
+              },
+            ]}
+          />
+        </BrowserRouter>
+      </Provider>
     );
     const title = getByText("Just Mercy");
     const rating = getByText("7.5");
-    const rating2 = getByText("out of 10");
     expect(title).toBeInTheDocument();
     expect(rating).toBeInTheDocument();
-    expect(rating2).toBeInTheDocument();
   });
 });
