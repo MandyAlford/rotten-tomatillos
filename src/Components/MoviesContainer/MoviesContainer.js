35d12a4777981a5ea5a85b24b0bac83a59615e90
Edward Stoner <edwardlstoner@gmail.com>
2020-04-13 20:27:49 -0600

    Add sort function

--- src/Components/MoviesContainer/MoviesContainer.js
+++ src/Components/MoviesContainer/MoviesContainer.js
@@ -4,7 +4,28 @@ import "./MoviesContainer.css";
 import MovieDetails from "../MovieDetails/MovieDetails";
 import { connect } from "react-redux";
 
-export const MoviesContainer = ({ movies }) => {
+const getDate = (movie, user) => {
+  const rating = user.ratings.find((rating) => rating.movie_id === movie.id);
+  return rating ? new Date(rating.created_at) : undefined;
+};
+
+const sortByDateRated = (movies, user) => {
+  return movies.sort((prevMovie, nextMovie) => {
+    const prevMovieDate = getDate(prevMovie, user);
+    const nextMovieDate = getDate(nextMovie, user);
+    if (!prevMovieDate) {
+      return 1;
+    }
+    return nextMovieDate - prevMovieDate;
+  });
+};
+
+export const MoviesContainer = ({ movies, user }) => {
+  if (user.name) {
+    movies = sortByDateRated(movies, user);
+  } else {
+    movies = movies.sort((a, b) => a.id - b.id);
+  }
   const cards = movies.map((movie) => {
     return <MovieCard key={movie.id} movie={movie} />;
   });
@@ -13,6 +34,7 @@ export const MoviesContainer = ({ movies }) => {
 
 const mapStateToProps = (state) => ({
   movies: state.movies,
+  user: state.user,
 });
 
 export default connect(mapStateToProps)(MoviesContainer);
